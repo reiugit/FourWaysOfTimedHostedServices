688adea0fbcb000acf01727ce9974f8917ba8958
rei <67372053+reiugit@users.noreply.github.com>
2024-06-30 19:33:47 +0200

    Added 'TimeProvider.System' to DI container.

--- FourWaysOfTimedHostedServices/Program.cs
+++ FourWaysOfTimedHostedServices/Program.cs
@@ -2,10 +2,12 @@ using FourWaysOfTimedHostedServices.HostedServices;
 
 var builder = Host.CreateApplicationBuilder(args);
 {
+    builder.Services.AddSingleton(TimeProvider.System);
+
     builder.Services.AddHostedService<WorkerWithTaskDelay>();
     builder.Services.AddHostedService<WorkerWithPeriodicTimer>();
     builder.Services.AddHostedService<HostedServiceWithTimer>();
-    builder.Services.AddHostedService(_ => new HostedServiceWithTimeProvider(TimeProvider.System));
+    builder.Services.AddHostedService< HostedServiceWithTimeProvider>();
 }
 
 var host = builder.Build();
